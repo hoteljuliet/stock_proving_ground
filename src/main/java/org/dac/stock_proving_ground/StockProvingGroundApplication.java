e3a957d952ff1c033bf074f4b029b23f3786f147
Colasurdo, Dennis <Dennis_Colasurdo@cable.comcast.com>
2017-11-11 09:52:50 -0500

    Inital Checkin

---
+++ src/main/java/org/dac/stock_proving_ground/StockProvingGroundApplication.java
@@ -0,0 +1,12 @@
+package org.dac.stock_proving_ground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProvingGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockProvingGroundApplication.class, args);
+	}
+}
